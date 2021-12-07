b8d1c099f7fa46ddefc63f37206c4b9941c17b9d
meirianni <68259191+meirianni@users.noreply.github.com>
2021-12-07 20:38:40 +0700

    penambahan dom

---
+++ ext.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  alert("New Arrival on December 2021, OFF 20%");
+}
+const cards = document.getElementsByClassName("card");
+// const card = document.querySelectorAll(".card-baby1");
+cards[0].style.backgroundColor = "#7f5a83";
+
+const h5 = document.getElementById("card-title");
+h5.style.color = "white";
